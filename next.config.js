5986a53a6392db6a486dc8ac6d4da5adf4e474cc
xleonard1 <xavier.lnrd@gmail.com>
2022-07-31 23:24:58 -0400

    changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     env: {
         mongodburl: "mongodb://localhost:27017/fakeDB",
-    }
+    },
+    images: {
+        loader: 'akamai',
+        path: '',
+        },
+        basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+      
 };
